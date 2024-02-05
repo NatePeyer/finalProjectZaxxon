9d0c96e572fc762b5080882a6669cffae029c7ab
nathaniel.peyer24 <nathaniel.peyer24@d155.org>
2024-02-05 08:48:11 -0600

    first commit, started work on background graphics.

---
+++ src/IsometricGraphics.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class IsometricGraphics {
+    public static void gameScreen()
+    {
+        JFrame frame = new JFrame();
+        frame.setSize(400,300);
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        frame.setVisible(true);
+
+    }
+}
