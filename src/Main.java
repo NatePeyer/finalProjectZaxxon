b79754b81d1c2aef572f27588615a43b3beeb0b8
Nathaniel Peyer <101201931+NatePeyer@users.noreply.github.com>
2024-02-05 14:39:16 -0600

    Update Main.java
    figured out what key does what

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,9 @@ public class Main {
         System.out.println("Hello world!");
     }
     IsometricGraphics.gameScreen();
+    // / - shot
+    // up arrow - go up
+    // down arrow - go down
+    // left arrow - go left
+    // right arrow - go right
 }
