a7f66f060beda1adb39c3e3fde02d427d4007af4
nathaniel.peyer24 <nathaniel.peyer24@d155.org>
2024-02-28 08:07:12 -0600

    started work on the spaceship

--- src/Spaceship.java
+++ src/Spaceship.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class Spaceship {
+
+    public Spaceship(JPanel panel)
+    {
+        //need to find a good picture of the spaceship, and then figure out how to set its start postion
+        // up and down arrow keys impact y axis
+        //left and right move side to side this get hard with iso
+        // /-key is shot
+
+    }
 }
