ed01c133196152aac56a0efe48bde35c5afbbcad
nathaniel.peyer24 <nathaniel.peyer24@d155.org>
2024-04-26 07:52:16 -0500

    started work on turret

--- src/Turret.java
+++ src/Turret.java
@@ -1,2 +1,16 @@
-public class Turret {
+public class Turret
+{
+    protected int x;
+    protected int y;
+
+    public Turret()
+    {
+        System.out.println("Turret Created");
+    }
+
+    public void turretHit()
+    {
+        //needs implmentation
+    }
+
 }
